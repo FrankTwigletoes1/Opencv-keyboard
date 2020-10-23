39b151558eacb4a8a1bdcbc932ce3961c10ccbae
Lunar <Lunar@DESKTOP-D2QOC4E>
2020-10-23 02:34:39 +0200

    yes

--- opencv camera.py
+++ opencv camera.py
@@ -13,7 +13,7 @@ class Detection(object):
         self.previous_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
  
     def get_active_cell(self, image):
-        # obtain motion between previous and current image
+
         current_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         delta = cv2.absdiff(self.previous_gray, current_gray)
         threshold_image = cv2.threshold(delta, 25, 255, cv2.THRESH_BINARY)[1]
@@ -24,14 +24,10 @@ class Detection(object):
         
         cv2.waitKey(1)
  
-        # store current image
+
         self.previous_gray = current_gray
  
-        # set cell width
-        height, width = threshold_image.shape[:2]
-        cell_width = int(width/7)
- 
-        # store motion level for each cell
+
         cells = np.array([0, 0, 0, 0, 0, 0, 0])
         cells[0] = cv2.countNonZero(threshold_image[0:200, 50:125])
         cells[1] = cv2.countNonZero(threshold_image[0:200, 125:200])
@@ -44,7 +40,7 @@ class Detection(object):
         top_cell =  np.argmax(cells)
 
 
-        # return the most active cell, if threshold met
+
         if(cells[top_cell] >= self.THRESHOLD):
             #print("play: ",top_cell)
             return top_cell
